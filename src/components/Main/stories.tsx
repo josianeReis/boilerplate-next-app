98b89120593b9065da8323c84531f6a1bb285bdf
JosianeReis <josiane-reis@outlook.com>
2023-09-06 13:03:40 +0100

    configs storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,12 @@
+import { StoryObj, Meta } from '@storybook/react';
+import Main from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta;
+
+export const Default: StoryObj = {};
